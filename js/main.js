dead9386be41d3a593e185a2a310e2b4c5ccd78e
Anna Edmund <annecreeyates@gmail.com>
2022-08-31 22:13:31 +0100

    in main.js: added dynamic class assignement to div

--- js/main.js
+++ js/main.js
@@ -9,28 +9,30 @@ for (let i = 0; i < 101; i++) {
 
 //integers should be the height of bars
 const barHeight = arrayForSorting;
-console.log(barHeight);
 
-//create 100 div elements
+//create 100 div elements --at the moment only 1 element is created, which contains a string of 100 numbers
 document.body.onload = addElement;
 
 function addElement () {
+    //create a loop to make 100 divs
+    for (let i = 0; i < arrayForSorting.length; i++) {
+    
     //create  a new div element
     const newDiv = document.createElement("div");
 
     //give it content (most likely in this case it will have to be a loop throufh the array?)
-    const newElement = document.createTextNode(barHeight);
+    const newElement = document.createTextNode(arrayForSorting[i]);
 
-    ////add the text node to the newly created div
+    //add the text node to the newly created div
     newDiv.appendChild(newElement);
 
     //add the new div and its content to DOM
     const currentDiv = document.getElementById("bar-components");
-    currentDiv.className = "bar-style";
+
+    //line below is supposed to add class name to each div -- doesn't work yet
+    newDiv.classList.add("bar-style");
+
     document.body.insertBefore(newDiv, currentDiv);
-
-    
-
-    //is supposed to add class to the div, but doesn't work
-    
+    } 
+        
 }
