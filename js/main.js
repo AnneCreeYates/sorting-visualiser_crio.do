338c5967c29248bf12f50cda92c4eb47ea869901
Anna Edmund <annecreeyates@gmail.com>
2022-10-09 20:02:18 +0100

    in CSS: changed the size of the buttons - they still are temporary; in JS: moved currentDiv variable outside the bars function, added addingBars function (probably will be removed- it copies what bars originally did), added removingBars function (contains only currentDiv.remove() at the moment - may be removed later, placed arrayForSorting variable and loop pushing the numbers to the array inside the bars function, initialised the bars function at the bottom (outside of the bars function)

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
+
+//create an event listener for the new array, so there is no need for reloading the page every time
+const newArrButton = document.getElementById("new-arr-btn");
+
+//creates 100 div elements on load
+// sdocument.body.onload = bars;
+
+//leaving the arrayForSorting, currentDiv nad the loop causes the bars not to be random after pressing the new array button the second time
+
 //create an array and push 100 numbers
-const arrayForSorting = [];
+// const arrayForSorting = [];
+    
+//add the new div and its content to DOM
+const currentDiv = document.getElementById("bar-components");
 
-//pushes 100 random numbers to the aarrayForSorting
-for (let i = 0; i < 101; i++) {
-    arrayForSorting.push(Math.floor(Math.random() * 101));
-}
-
-//integers should be the height of bars
-// const barHeight = arrayForSorting;
-
-//create 100 div elements 
-document.body.onload = bars;
+//pushes 100 random numbers to the aarrayForSorting - works here better than up top
+// for (let i = 0; i < 101; i++) {
+//     arrayForSorting.push(Math.floor(Math.random() * 101));
+// }
 
 
-//the function creates 100 divs, adds new divs to DOM, adds bar-style class to each div
-function bars () {
-    //add the new div and its content to DOM
-    const currentDiv = document.getElementById("bar-components");
+//the below kinda works, but adds the visualisation on new bars instead of replacing the old one
+//also adds identical one instead of random
 
-    //create a loop to make 100 divs
+// newArrButton.addEventListener("click", bars);
+newArrButton.addEventListener("click", removingBars);
+
+//function adding new random bars
+
+function addingBars() {
+    //create a loop to make 100 divs - the same loop is in the bar function (does the same) - there the bars are rundom after pressing the second time
     for (let j = 0; j < arrayForSorting.length; j++) {
     
         //create a new div element for each iteration
@@ -28,25 +38,91 @@ function bars () {
         // const newElement = document.createTextNode(arrayForSorting[j]);
 
         //add the text node to the newly created div
-        // newDiv.appendChild(newElement);
-
         currentDiv.appendChild(newDiv);
 
-        //width of the div is to be determined by the array value
+        //height of the div is to be determined by the array value
         newDiv.style.height = arrayForSorting[j] + '%';
 
-
-        //line below adds class name to each div --- when added to the currentDiv it work on the general container not the elements
-        //adding to newDiv adds the style to the elements
+        //line below adds class name to each div
         newDiv.classList.add("bar-style");
-        
-        //inserts the new divs in front of the bar-component div 
-        //--- may need to figure out how to make the dynamic divs go inside the bar-components div
-        // document.body.insertBefore(newDiv, currentDiv);
 
-
-        /*when lines 31 and 34 and 37 are changed to currentDiv a line of numbers appears in a row -- experiment with that */
-
-    }        
+    }
 }
 
+function removingBars (){
+    // for (let k = -1; k < arrayForSorting.length; k--) {
+
+        // const newDiv = document.getElementsByClassName("bar-style");
+
+        //successfuly removes the entire section
+        currentDiv.remove();
+
+        
+        // for (let k = 0; k = currentDiv.length - 1; k--){
+        //     currentDiv.remove(newDiv);
+            
+        // }
+        
+        // console.log(newDiv);
+        // console.log(currentDiv);
+        // console.log(arrayForSorting)
+        
+        //height of the div is to be determined by the array value
+        // newDiv.style.height = 0;
+
+        //line below adds class name to each div
+        // newDiv.classList.add("bar-style");
+
+       
+
+    // }
+    
+}
+
+
+//the function creates 100 divs, adds new divs to DOM, adds bar-style class to each div
+function bars () {
+
+        
+    // //create an array and push 100 numbers
+    const arrayForSorting = [];
+    
+    // //add the new div and its content to DOM
+    // const currentDiv = document.getElementById("bar-components");
+    
+    // //pushes 100 random numbers to the aarrayForSorting - works here better than up top
+    for (let i = 0; i < 101; i++) {
+        arrayForSorting.push(Math.floor(Math.random() * 101));
+        
+    }
+
+    for (let j = 0; j < arrayForSorting.length; j++) {
+    
+        //create a new div element for each iteration
+        const newDiv = document.createElement("div");
+
+        //give each div content -- prints put the numbers from the array one next to the other -- works without it
+        // const newElement = document.createTextNode(arrayForSorting[j]);
+
+        //add the text node to the newly created div
+        currentDiv.appendChild(newDiv);
+
+        //height of the div is to be determined by the array value
+        newDiv.style.height = arrayForSorting[j] + '%';
+
+        //line below adds class name to each div
+        newDiv.classList.add("bar-style");
+
+    }
+
+   
+    // console.log(currentDiv)
+    // currentDiv.remove(document.getElementsByClassName("bar-style"));   
+   
+    // removingBars();
+    // addingBars();
+   
+};
+
+//initiates the function on load
+bars();
